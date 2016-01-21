da03cf5a9c290e8eb69d41f4a171fb4b835b3d26
Stanley Quon <stanleyq@yahoo.com>
2016-01-21 22:46:08 +0000

    Even more changes

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,17 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+  # it 'does something' do
+    # skip 'Replace this with meaningful tests'
+  # end
+   describe package ('emacs') do
+      it {should be_installed}
+   end
+   describe package ('tree') do
+      it {should be_installed}
+   end
+   describe file ("/etc/motd") do
+      its (:content) {should match /Property of ...\n/}
+      # it {should contain /Property of/}
+   end
 end
