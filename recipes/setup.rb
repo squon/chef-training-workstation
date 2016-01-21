1a00ba2d870c3becfe5f9b6117648bdfd6688e9d
Stanley Quon <stanleyq@yahoo.com>
2016-01-21 22:50:36 +0000

    Yet more changes

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,9 +3,9 @@ package 'emacs'
 # package 'vim'
 package 'tree'
 package 'git'
-# file '/etc/motd' do
-#   content "Property of ...\n"
-#   mode '0644'
-#   owner 'root'
-#   group 'root'
-# end
+file '/etc/motd' do
+  content "Property of ...\n"
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
