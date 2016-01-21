3de555b886d5fb5fb26b76ba3a3dcf4974dc1274
ChefDK User <chef@ip-172-31-14-126.ec2.internal>
2016-01-21 19:25:22 +0000

    Initial workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,11 @@
+# package 'nano'
+# package 'emacs'
+# package 'vim'
+# package 'tree'
+package 'git'
+# file '/etc/motd' do
+#   content "Property of ...\n"
+#   mode '0644'
+#   owner 'root'
+#   group 'root'
+# end
